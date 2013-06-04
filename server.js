76c5e449da942b02db24b753d87893f58f422b0b
Jason Lally <jason.lally@gmail.com>
2013-06-04 12:03:35 -0600

    remove node and npm config from package.json

--- server.js
+++ server.js
@@ -13,4 +13,4 @@ app.get('/blockgroup/:id/:field', laidata.getFieldById);
 app.get('/blockgroups', laidata.getBlockgroups);
 
 app.listen(8080);
-console.log('Listening on port 3000...');
+console.log('Listening on port 8080...');

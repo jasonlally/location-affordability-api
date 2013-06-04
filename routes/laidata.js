27c66e4163ed32d36b951ed6e78ee416fa8f7f2c
Jason Lally <jason.lally@gmail.com>
2013-06-03 23:04:38 -0600

    add api calls for data

---
+++ routes/laidata.js
@@ -0,0 +1,50 @@
+var mongo = require('mongodb');
+
+var Server = mongo.Server,
+    Db = mongo.Db,
+    BSON = mongo.BSONPure;
+
+var server = new Server('localhost', 27017, {auto_reconnect: true});
+db = new Db('lai', server);
+
+db.open(function(err, db) {
+    if(!err) {
+        console.log("Connected to 'lai' database");
+        db.collection('blkgrps', {strict:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'blockgroups' collection doesn't exist. Creating it with sample data...");
+            }
+        });
+    }
+});
+
+exports.findById = function(req, res) {
+    var id = req.params.id;
+    console.log('Retrieving blockgroup: ' + id);
+    db.collection('blkgrps', function(err, collection) {
+        collection.findOne({ 'blkgrp' : "'" + id + "'"}, function(err, item) { //TODO: remove single quotes from database later
+            res.send(item);
+        });
+    });
+};
+
+exports.getFieldById = function(req, res) {
+    console.log(req);
+    var field = req.params.field;
+    var id = req.params.id;
+    console.log('Retrieving ' + field + ' for blockgroup' + id);
+    db.collection('blkgrps', function(err, collection) {
+        collection.findOne({ 'blkgrp' : "'" + id + "'"}, {fields : [field, 'blkgrp']}, function(err, item) {
+           res.send(item);
+        });
+    });
+}
+
+exports.getBlockgroups = function(req, res) {
+    console.log('Retrieving blockgroup ids');
+    db.collection('blkgrps', function(err, collection) {
+        collection.find({},{fields : ['blkgrp']}).toArray(function(err, items) {
+            res.send(items);
+        });
+    });
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    laidata = require('./routes/laidata');
+
+var app = express();
+
+app.configure(function () {
+    app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser());
+});
+
+app.get('/blockgroup/:id', laidata.findById);
+app.get('/blockgroup/:id/:field', laidata.getFieldById);
+app.get('/blockgroups', laidata.getBlockgroups);
+
+app.listen(3000);
+console.log('Listening on port 3000...');

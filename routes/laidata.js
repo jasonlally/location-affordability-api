8eafe026cf580c6bb2761d1d1bd1bc968686314c
Jason Lally <jason.lally@gmail.com>
2013-06-04 11:51:10 -0600

    add dotcloud server variables and authentication

--- routes/laidata.js
+++ routes/laidata.js
@@ -4,21 +4,34 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
+var pass, user;
+
 if (process.env.NODE_ENV == 'production') {
     var env = JSON.parse(fs.readFileSync('/home/dotcloud/environment.json', 'utf-8'));
-    var server = new Server(env['DOTCLOUD_DATA_MONGODB_URL'], env['DOTCLOUD_DATA_MONGODB_PORT'], {auto_reconnect: true});
+    var server = new Server(env['DOTCLOUD_DATA_MONGODB_HOST'], env['DOTCLOUD_DATA_MONGODB_PORT'], {auto_reconnect: true});
+    user = env['DOTCLOUD_DATA_MONGODB_LOGIN'];
+    pass = env['DOTCLOUD_DATA_MONGODB_PASSWORD'];
 } else {
-    var server = new Server('localhost', 27017, {auto_reconnect: true});
+    var server = new Server('localhost', 27017, {auto_reconnect: true}); //27017
+    user = '';
+    pass = '';
 }
 
 db = new Db('lai', server);
 
+/*
+db.authenticate('root', 'B13zimMS52bVstZGcIXh', function(err, res) {
+    if (err) console.log(err);
+}); */
+
 db.open(function(err, db) {
     if(!err) {
         console.log("Connected to 'lai' database");
+        db.authenticate(user,pass);
         db.collection('blkgrps', {strict:true}, function(err, collection) {
             if (err) {
                 console.log("The 'blockgroups' collection doesn't exist. Creating it with sample data...");
+                //populateDB();
             }
         });
     }
@@ -50,7 +63,37 @@ exports.getBlockgroups = function(req, res) {
     console.log('Retrieving blockgroup ids');
     db.collection('blkgrps', function(err, collection) {
         collection.find({},{fields : ['blkgrp']}).toArray(function(err, items) {
+            if(err) console.log(err);
             res.send(items);
         });
     });
+};
+
+
+var populateDB = function() {
+
+    var wines = [
+        {
+            name: "CHATEAU DE SAINT COSME",
+            year: "2009",
+            grapes: "Grenache / Syrah",
+            country: "France",
+            region: "Southern Rhone",
+            description: "The aromas of fruit and spice...",
+            picture: "saint_cosme.jpg"
+        },
+        {
+            name: "LAN RIOJA CRIANZA",
+            year: "2006",
+            grapes: "Tempranillo",
+            country: "Spain",
+            region: "Rioja",
+            description: "A resurgence of interest in boutique vineyards...",
+            picture: "lan_rioja.jpg"
+        }];
+
+    db.collection('wines', function(err, collection) {
+        collection.insert(wines, {safe:true}, function(err, result) {});
+    });
+
 };
